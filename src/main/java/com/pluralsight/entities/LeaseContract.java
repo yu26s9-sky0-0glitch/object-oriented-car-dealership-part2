6298fc553e07a481c6c01a64e1818d7011cd4cc1
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2026-05-17 22:24:03 -0400

    added lease override classes as well as removing and adding new instances to make the logic work

--- src/main/java/com/pluralsight/entities/LeaseContract.java
+++ src/main/java/com/pluralsight/entities/LeaseContract.java
@@ -1,38 +1,43 @@
 package com.pluralsight.entities;
 
 public class LeaseContract extends Contract {
-    private double expectedEndingValue;
-    private double leaseFee;
+    private double originalPrice;
 
-    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, double expectedEndingValue, double leaseFee) {
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, double originalPrice) {
         super(date, customerName, customerEmail, vehicleSold);
-        this.expectedEndingValue = expectedEndingValue;
-        this.leaseFee = leaseFee;
+        this.originalPrice = getVehicleSold().getPrice();;
+    }
+
+    public double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(double originalPrice) {
+        this.originalPrice = originalPrice;
     }
 
     public double getExpectedEndingValue() {
-        return expectedEndingValue;
+        return this.originalPrice * 0.5;
     }
 
-    public void setExpectedEndingValue(double expectedEndingValue) {
-        this.expectedEndingValue = expectedEndingValue;
-    }
 
     public double getLeaseFee() {
-        return leaseFee;
-    }
-
-    public void setLeaseFee(double leaseFee) {
-        this.leaseFee = leaseFee;
+        return this.originalPrice*0.07;
     }
 
     @Override
     public double getTotalPrice() {
-        return 0;
+        return (this.originalPrice - getExpectedEndingValue()) + getLeaseFee();
     }
 
     @Override
     public double getMonthlyPayment() {
-        return 0;
+        double annualRate = 0.04;
+        double monthlyRate = annualRate / 12;
+        int months = 36;
+
+        double loanAmount = getTotalPrice();
+        return (loanAmount * monthlyRate * Math.pow(1 + monthlyRate, months)) /
+                (Math.pow(1 + monthlyRate, months) - 1);
     }
 }
